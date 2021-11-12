ff3ca9e05a348eb372eb5f0110fbf23548557155
Jiarui Chen <jchen324@jhu.edu>
2021-11-11 22:49:02 -0500

    add comments

--- magic.cpp
+++ magic.cpp
@@ -31,14 +31,14 @@ using std::endl;
 
 
 int main(int argc, char **argv) {
-  char* filename = argv[1];
+  char* filename = argv[1];   // get the filename
   
   if (argc != 2) {
     cout << "Wrong command argument" << endl;
     return 1;
   }
 
-  int fd = open(filename, O_RDONLY);
+  int fd = open(filename, O_RDONLY);  
 
   if (fd < 0) {
     // error
@@ -60,14 +60,16 @@ int main(int argc, char **argv) {
   void *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
   if (data == (void*)-1) {
+    // error with memory mapping
     cout << "Error with memory mapping" << endl;
     return 1;
   }
 
 
-  Elf64_Ehdr *elf_header = (Elf64_Ehdr *) data;
+  Elf64_Ehdr *elf_header = (Elf64_Ehdr *) data;   // data is a pointer to the beginning of the ELF file data in memory
 
   if (!(elf_header->e_ident[0] == 0x7f && elf_header->e_ident[1] == 'E' && elf_header->e_ident[2] == 'L' &&  elf_header->e_ident[3] == 'F' )) {
+    // use four bytes to check if the file is an ELF file
     cout << "Not an ELF file" << endl;
     exit(0);
   }
@@ -96,21 +98,24 @@ int main(int argc, char **argv) {
     printf("size=%lx", curr_section->sh_size);  // find size
     printf("\n");
 
+    // compare section names to locate symbol table and string table sections
     if (strcmp((char*)(data + section_name_table->sh_offset + curr_section->sh_name), ".symtab") == 0) symtab = curr_section;
     if (strcmp((char*)(data + section_name_table->sh_offset + curr_section->sh_name), ".strtab") == 0) strtab = curr_section;
   }
 
+  // calculate total entries in symbol table
   Elf64_Xword entry_size = symtab->sh_entsize;
   Elf64_Xword total_size = symtab->sh_size;
   Elf64_Xword total_entry = total_size/entry_size;
  
+  // iterate through all entries in symbol table
   for (int i=0; i< (int)total_entry; i++) {
-    Elf64_Sym *symtab_object = (Elf64_Sym *)((unsigned char*)data + symtab->sh_offset + i*entry_size);
+    Elf64_Sym *symtab_object = (Elf64_Sym *)((unsigned char*)data + symtab->sh_offset + i*entry_size);    // pointer to Elf64_Sym object in symble table
     printf("Symbol %d: ", i);
-    printf("name=%s, ", (char *)data + strtab->sh_offset + symtab_object->st_name);
-    printf("size=%lx, ", (long unsigned int)symtab_object->st_size);
-    printf("info=%lx, ", (long unsigned int)symtab_object->st_info);
-    printf("other=%lx", (long unsigned int)symtab_object->st_other);
+    printf("name=%s, ", (char *)data + strtab->sh_offset + symtab_object->st_name);   // find name based on st_name offset
+    printf("size=%lx, ", (long unsigned int)symtab_object->st_size);   // size
+    printf("info=%lx, ", (long unsigned int)symtab_object->st_info);   // info
+    printf("other=%lx", (long unsigned int)symtab_object->st_other);   // other
     printf("\n");
   }
 }
